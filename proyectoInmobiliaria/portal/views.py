3309fd87052b2a1a1a9469c9fb2564f2868b4167
jazbar9 <jazmina.sanz@gmail.com>
2026-03-26 11:06:38 -0500

    Views, Ruta, Template index.html

--- proyectoInmobiliaria/portal/views.py
+++ proyectoInmobiliaria/portal/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Inmueble 
 
 # Create your views here.
+def lista_inmuebles(request):
+    inmuebles = Inmueble.objects.all()  #select * from portal_nmuebles
+    print(inmuebles)
+    context = {
+        'inmuebles':inmuebles
+    }
+    
+    return render(request, 'portal/index.html', context)
